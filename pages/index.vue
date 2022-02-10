a35179fe9161b74c95644ee3130277215c169604
Jonathan Chue <5141427+jchue@users.noreply.github.com>
2022-02-09 17:41:31 -0800

    Fix font import

--- pages/index.vue
+++ pages/index.vue
@@ -84,8 +84,6 @@ export default Vue.extend({
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Roboto+Condensed&family=Source+Sans+Pro&display=swap');
-
 $breakpoint-small: 400px;
 $breakpoint-med: 480px;
 
